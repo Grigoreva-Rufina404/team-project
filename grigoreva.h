194d5a8164bb9bb130a664ee2c145c7de7db8ec0
Grigoreva Rufina <gr1gruu@gmail.com>
2026-09-18 22:46:11 +0600

    Добавлен заголовочный файл grigoreva.h с прототипами функций варианта 24

---
+++ grigoreva.h
@@ -0,0 +1,6 @@
+#ifndef GRIGOREVA_H
+#define GRIGOREVA_H
+// Функции варианта 24 (Григорьева Р. И.)
+double kineticEnergy(double m, double v);
+double momentum(double m, double v);
+#endif // GRIGOREVA_H
